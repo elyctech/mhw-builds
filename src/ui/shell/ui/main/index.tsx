f65606d53b12bee91bc2d7e1af7b342fb148074c
Cyle Ven Dawson <dawsonc623@gmail.com>
2020-09-25 15:49:16 -0500

    feat: add in home route and basic MDC scaffolding

--- src/ui/shell/ui/main/index.tsx
+++ src/ui/shell/ui/main/index.tsx
@@ -2,9 +2,67 @@ import React, {
   ReactElement
 } from "react";
 
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from "react-router-dom";
+
+import {
+  ThemeProvider
+} from "@rmwc/theme";
+
+import {
+  TopAppBar,
+  TopAppBarFixedAdjust,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle
+} from "@rmwc/top-app-bar";
+
+import "@rmwc/top-app-bar/styles";
+
 export default function ShellMain() : ReactElement
 {
+  const renderHome  = ()  : ReactElement =>
+  {
+    return (
+      <p>Hello, React, Parcel, and Material Design!</p>
+    );
+  };
+
   return (
-    <p>Hello React and Parcel!</p>
+    <div
+      className = "shell"
+    >
+      <ThemeProvider
+        options = {{
+          "primary"   : "#444",
+          "secondary" : "#333"
+        }}
+
+        style = {{
+          "flex" : 1
+        }}
+      >
+        <TopAppBar>
+          <TopAppBarRow>
+            <TopAppBarSection>
+              <TopAppBarTitle>MHW Builds</TopAppBarTitle>
+            </TopAppBarSection>
+          </TopAppBarRow>
+        </TopAppBar>
+        <TopAppBarFixedAdjust />
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact   = {true}
+              path    = "/"
+              render  = {renderHome}
+            />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
+    </div>
   );
 }
